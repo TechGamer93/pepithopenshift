f0e92adabc69a5c16e85c444224ac0d76184d3f7
TechGamer93 <ernestomrnd92@gmail.com>
2015-12-14 15:33:07 -0700

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "adminbdAZ1fs"); // db user
+define('DB_PASSWORD', "wtWtQTq3WwiK"); // db password (mention your db password here)
+define('DB_DATABASE', ""); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
